d27e6a858816f4910bd590a19da35037f7ed56e3
ImBigman <b1gmanever@gmail.com>
2019-10-15 20:40:03 +0300

    правки отступов и последней строки

--- lesson1/1st.rb
+++ lesson1/1st.rb
@@ -1,10 +1,10 @@
 #Идеальный вес
-puts "Как вас зовут? :"
-name = gets.chomp.to_i
-puts "Какой у вас рост? :"
+puts 'Как вас зовут? :'
+name = gets.chomp
+puts 'Какой у вас рост? :'
 height = gets.chomp.to_i
 if (height - 110) > 0
-  puts "Ваш идеальный вес, #{name} : #{height - 110}" 
-else 
-  puts "Ваш вес уже оптимальный"
+  puts "Ваш идеальный вес, #{name} : #{height - 110}"
+else
+  puts 'Ваш вес уже оптимальный'
 end
