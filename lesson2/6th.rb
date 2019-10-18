94f40bf309ee3e4410949f9f72f0dd055a108922
ImBigman <b1gmanever@gmail.com>
2019-10-18 17:12:04 +0300

    closer

---
+++ lesson2/6th.rb
@@ -0,0 +1,21 @@
+#Сумма покупок.
+goods = Hash.new{}
+goods_sum = Hash.new{}
+to_pay = 0
+loop do
+  puts "Введите название товара: "
+  items = gets.chomp.to_sym
+  if items == :стоп
+    break   
+  end
+  puts "Введите цену товара: "
+  price = gets.chomp.to_f
+  puts "Введите количество товара: "
+  count = gets.chomp.to_f
+  goods[items] = { цена: price, количество: count } 
+  goods_sum[items] = price * count
+  to_pay = goods_sum.sum { |k, v| v}
+end
+puts "Наименование, цена за единицу, количество: " "#{goods}" 
+puts "Сумма по видам товаров: " "#{goods_sum}" 
+puts "К оплате: " "#{to_pay}"
