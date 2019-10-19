24472a2468e91f9705c7a4f14a2130eb05a51c9c
ImBigman <b1gmanever@gmail.com>
2019-10-19 10:24:18 +0300

    fix

--- lesson2/4th.rb
+++ lesson2/4th.rb
@@ -1,9 +1,9 @@
-#Заполнить хеш гласными буквами,
+ #Заполнить хеш гласными буквами,
 #где значением будет являтся порядковый номер буквы в алфавите (a - 1).
-abc = ("а".."я").to_a
-vowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"]
-abc_index = []
-abc_index = abc.to_enum(:each_with_index).map { |i, a| [a + 1, i] }
-abc_hash = Hash[*abc_index.flatten(1)]
-abc_hash_vowels = abc_hash.select { |num, letter| letter if vowels.include?(letter) }
-puts abc_hash_vowels = abc_hash_vowels.invert
+abc = ('а'..'я').to_a
+vowels = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я']
+vowels_hash = {}
+abc.each_with_index do |letter, index|
+  vowels_hash[letter] = index + 1 if vowels.include?(letter)
+end
+puts vowels_hash
